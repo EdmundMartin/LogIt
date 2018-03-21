a0fbdbd99ad4eefd47f14f3e6d666f7ea1f6f3d1
EdmundMartin <edmartin101@googlemail.com>
2018-03-21 22:15:23 +0000

    pep8 and some refactoring

--- logit.py
+++ logit.py
@@ -1,17 +1,15 @@
-import multiprocessing
-from multiprocessing import Pool
+from multiprocessing import cpu_count, Pool
 import time
 import csv
-import datetime
 
-# Multiprocessing example
 
-class logger_multi(object):
+class LogitMultiprocessing(object):
 
-    def __init__(self,user_agent,ips=None):
-        self.user_agent = user_agent # List of user agents we want to validate
-        self.ips = ips # IP's in list
+    def __init__(self, user_agents, ips=None, processes=None):
+        self.user_agents = user_agents  # List of user agents we want to validate
+        self.ips = ips  # IP's in list
         self.results = []
+        self.processes = cpu_count() if not processes else processes
 
     def __date_clean(self, dirty_date):
         date_tuple = time.strptime(dirty_date, "%d/%b/%Y")
@@ -21,25 +19,22 @@ class logger_multi(object):
         time_split = item.split(':')
         return self.__date_clean(time_split[0]), ':'.join(time_split[1:])
 
-    def verify(self, UA, IP):
-        for user in self.user_agent:
-            if user in UA:
-                for ip in self.ips:
-                    if IP.startswith(ip):
-                        return True
-                    else:
-                        continue
+    def verify(self, ua, current_ip):
+        if any(i in ua for i in self.user_agents):
+            for ip in self.ips:
+                if current_ip.startswith(ip):
+                    return True
         return False
 
-    def open_file(self,filename):
-        file = open(filename,encoding="utf-8")
+    def open_file(self, filename):
+        file = open(filename, encoding="utf-8")
         return file
 
     def chunks(self, l, n):
         for i in range(0, len(l), n):
             yield l[i:i + n]
 
-    def parse_chunk(self,data):
+    def parse_chunk(self, data):
         results = []
         for line in data:
             try:
@@ -50,23 +45,22 @@ class logger_multi(object):
                 parsed['status'] = line[8]
                 parsed['request_size'] = line[9]
                 parsed['user_agent'] = ' '.join(line[11:])
-                parsed['verify'] = str(self.verify(parsed['user_agent'],parsed['ip']))
+                parsed['verified'] = str(self.verify(parsed['user_agent'], parsed['ip']))
                 results.append(parsed)
             except Exception as e:
                 print(e)
         return results
 
     def run_process(self, file):
-        file = self.open_file(file).readlines()
-        listify = [line.split() for line in file]
+        listify = [line.split() for line in self.open_file(file).readlines()]
 
-        data = self.chunks(listify, int(len(listify) / (multiprocessing.cpu_count() - 2)))
-        p = Pool(processes=multiprocessing.cpu_count() - 2)
+        data = self.chunks(listify, int(len(listify) / self.processes))
+        p = Pool(processes=self.processes)
         results = [p.apply_async(self.parse_chunk, args=(list(x),)) for x in data]
 
         # wait for results
         results = [item.get() for item in results]
-        self.results = sum(results, [])
+        self.results = [item for sublist in results for item in sublist]
 
     def results_to_csv(self, output_file):
         headers = self.results[0].keys()
@@ -78,8 +72,9 @@ class logger_multi(object):
     def clear_results(self):
         self.results = []
 
+
 if __name__ == '__main__':
-    c = logger_multi(user_agent=['Googlebot','Mediapartners-Google'],
+    c = LogitMultiprocessing(user_agents=['Googlebot','Mediapartners-Google'],
                      ips=['64.233', '66.102', '66.249', '72.14', '74.125', '209.85', '216.239', '66.184'])
-    c.run_process('log-new.txt')
+    c.run_process('logs.demo')
     c.results_to_csv('example-multi.csv')
